2c72b8af1e402db75bb3ddbe3e513ee28599744b
Your Name <your@example.com>
2022-04-14 04:05:01 +0000

    bug fix

--- src/main/java/com/example/Math.java
+++ src/main/java/com/example/Math.java
@@ -6,7 +6,7 @@ public class Math {
     }
 
     public int substract(int a, int b) {
-        return a - b + 1; /// coding fault
+        return a - b;
     }
 
     public int multiply(int a, int b) {
